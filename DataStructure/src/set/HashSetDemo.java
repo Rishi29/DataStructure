b0b497b969b47418882f80c0aaf3f946679d9a71
Rishi29 <me.rishi29@gmail.com>
2016-10-29 01:59:00 -0400

    Hashset Demo
    HashSet implements set( set interface implements Collection interface)
    
    hash set is developed based on the Hashtable datastructure
    
    Duplicate is not allowed, if added , no runtime or compile time
    exception thrown, but add method return false.
    
    Insertion order not preserved, objects will be inserted based on the
    hash code.
    
    Heterogenous object allowed
    
    null insertion is possible
    
    Hashset is serializable and clonable hence it is serializable and
    clonable
    
    Objects are stored on the basis of hashcode, hence search algorithm is
    easy.
    
    
    Note: if search operationis frequent then HashSet is the best choice
    
    
    four Constructor
    
    1) Hashset h= new Hashset();
    
    default initial capacity=16
    default fill ratio(load factor)=0.75
    
    2) Hashset h= new Hashset(int initialcapacity)
    
    customized initial capacity can be placed
    load factor= 0.75
    
    3) Hashset h= new Hashset(int initial capacity, float loadFactor)
    load factor can be customized as well
    
    4)Hashset h= new Hashset(Collection c )
    
    for inter conversion between collection objetcs
    
    
    
    LoadFactor/fill Ratio:
    
    After loading the how much factor  a new hasset object will be created
    that is called a load factor or fill ratio.

--- DataStructure/src/set/HashSetDemo.java
+++ DataStructure/src/set/HashSetDemo.java
@@ -1,5 +1,21 @@
 package set;
 
-public class HashSet {
+import java.util.HashSet;
+/*
+ * Hashset demo
+ * **/
+public class HashSetDemo {
+	
+public static void main(String[]args){
+	HashSet h= new HashSet();
+	h.add("B");
+	h.add("A");//String can be added
+	h.add("Z");
+	h.add(10);//Integer can be added
+	//since String, Integer and other objects can be added to same hashset so it accepts heterogeneous objects
+	h.add(null);//null can be added
+	System.out.println(h.add("A"));// will return false, Return type of add is boolean
+	System.out.println(h);
+}
 
 }
